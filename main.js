95a0eda4d737c20ddd47478c04bd9b7a2bf8e08e
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 23:13:55 +0100

    configuration

---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) {
+    msg.reply("pong");
+  }
+});
+
+client.login(TOKEN);
